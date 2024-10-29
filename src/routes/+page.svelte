bdcb3a444ef401ea84303d26da0f57b6e5d95280
Rajasekar P <rajasekar22@gmail.com>
2024-10-29 18:50:45 +0530

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    let count = $state(0);
+    let triple = $derived(count * 3);
+
+</script>
+
+<h1>count {count}, triple {triple}</h1>
+<button onclick={()=> count++}> increment </button>
+<button onclick={()=> count--}> decrement </button>
+
